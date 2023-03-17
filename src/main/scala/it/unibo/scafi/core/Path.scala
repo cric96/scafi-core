85e0b4646997f4ee0931e1452e4a3d358a4959d2
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-03-17 09:50:38 +0100

    chore: prefer to put everything inside Path

--- src/main/scala/it/unibo/scafi/core/Path.scala
+++ src/main/scala/it/unibo/scafi/core/Path.scala
@@ -7,7 +7,6 @@ trait Path {
   def isRoot: Boolean
   def head: Slot
   def path: List[Slot]
-
   def /(slot: Slot): Path = push(slot)
 }
 
@@ -40,4 +39,17 @@ class PathImpl(val path: List[Slot]) extends Path with Equals {
   override def hashCode(): Int = path.hashCode
 
   override def head: Slot = path.head
+}
+
+sealed trait Slot {
+  def ->(v: Any): (Path, Any) = (Path(this), v)
+  def /(slot: Slot): Path = Path(this, slot)
+}
+
+object Slot {
+  final case class Nbr(index: Int) extends Slot
+  final case class Rep(index: Int) extends Slot
+  final case class FunCall(index: Int, funId: Any) extends Slot
+  final case class FoldHood(index: Int) extends Slot
+  final case class Branch(index: Int, tag: Boolean) extends Slot
 }
